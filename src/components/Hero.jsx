c8cf1eb12a6cff143ac688a54b3393e812504de8
patricknambajimana <patricknambajimana6@gmail.com>
2025-08-11 17:07:08 +0200

    commited

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,19 +8,21 @@ function Hero() {
           hello i'm <strong>patrick nambajimana</strong>{" "}
         </p>
       </div>
-      <div className="flex gap-10 m-40">
-        <div className=" w-200 rounded-2xl shadow text-left text-2xl text-neutral-500 p-15">
+      <div className="flex gap-25 justify-center mt-20">
+        <div className="max-w-md rounded-2xl shadow text-left text-2xl text-neutral-500 p-5 ">
           I’m a dedicated Front-End Developer with a strong focus on creating
           modern, responsive, and high-performance web applications using React
           and TypeScript. I excel at building scalable, reusable UI components
           and delivering clean, maintainable code that meets both user and
           business needs. My expertise includes Context API, Tailwind CSS, and
           seamless REST API integration.
+          <span className="border border-cyan-400 capitalize bg-cyan-600 text-white p-2 mt-5">learn more</span>
+         </div>
+        <div className="rounded-2xl shadow text-center-safe">
+          <img src={image} alt="profile image"className=" p-5 " />
         </div>
-        <div className="rounded-2xl shadow ">
-          <img src={image} alt="profile image"className="text-center-safe pb-10" />
-        </div>
-      </div>
+      
+    </div>
     </div>
   );
 }
